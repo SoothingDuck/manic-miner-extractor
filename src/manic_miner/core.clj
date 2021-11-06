0d86f281fe9d41bb431d4ece38f961182a5d4b31
Yvan Aillet <ya186002@DESKTOP-IK2NI6H.localdomain>
2021-11-06 23:15:58 +0100

    some small changes

--- src/manic_miner/core.clj
+++ src/manic_miner/core.clj
@@ -4,12 +4,10 @@
   (:require [clojure.string :as s])
   (:require [manic-miner.const :as const])
   (:require [manic-miner.room :as room])
-  (:require [manic-miner.room :as file])
+  (:require [manic-miner.file :as file])
   (:require [manic-miner.tools :as tools]))
-
-(map room/name (room/data-list))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println (map room/name (room/data-list))))
