4e52942f4c7722ccfa58c3f920f79828f1a432f1
Yvan Aillet <yvanaillet@gmail.com>
2022-11-11 18:41:20 +0100

    le plan

--- src/manic_miner/core.clj
+++ src/manic_miner/core.clj
@@ -19,12 +19,48 @@
 (defn byte-list [len]
   (b/repeated :byte :length len))
 
+
+(def block-graphic
+  "Element d'affichage"
+  (b/ordered-map
+    :colour-attribute :byte
+    :pixel-pattern (b/repeated :byte :length 8)
+    )
+  )
+
+
+(def block-graphics 
+  "Les block graphics pour le background"
+  (b/ordered-map
+    
+    :background block-graphic
+    :floor block-graphic
+    :crumbling-floor block-graphic
+    :wall block-graphic
+    :conveyor block-graphic
+    :nasty-1 block-graphic
+    :nasty-2 block-graphic
+    :spare block-graphic
+    
+    )
+  )
+
 (def manic-miner-room
   "Description d'une chambre de manic miner"
   (b/ordered-map
-    :screen-layout (byte-list 512)
-    :room-name (b/string "US-ASCII" :length 32)
-    :the-rest (byte-list 480)
+    :screen-layout (byte-list 512) ;; 0 to 511
+    :room-name (b/string "US-ASCII" :length 32) ;; 512 to 543
+    :block-graphics block-graphics ;; 72 - 544 to 615
+    :miner-willy-start-position (byte-list 7) ;; 616 to 622
+    :conveyor (byte-list 4) ;; 623 to 626
+    :border-color :byte ;; 1 - 627
+    :items (byte-list 27) ;; 628 to 654
+    :portal (byte-list 37) ;; 655 to 691
+    :item-graphic (byte-list 8) ;; 692 to 699
+    :air (byte-list 2) ;; 700 to 701
+    :horizontal-guardians (byte-list 31) ;; 702 to 732
+    :vertical-guardians (byte-list 35) ;; 733 to 767
+    :guardian-graphics (byte-list 256) ;; 768 to 1023
     )
   )
 
@@ -43,10 +79,18 @@
   (let [in (io/input-stream filename)]
     (b/decode manic-miner-file in)))
 
-(let [decoded (decode-manic manic-miner-filename)
-      room-names-list (map :room-name (:rooms decoded))]
-  (doall
-    (map (comp println s/trim) room-names-list))
-)
 
+(def manic-miner-room-names
+  "Extrait les noms des pièces"
+  (let [decoded (decode-manic manic-miner-filename)
+        room-names-list (map :room-name (:rooms decoded))]
+    (map s/trim room-names-list)
+    )
+  )
 
+(def central-cavern
+  (let [decoded (decode-manic manic-miner-filename)]
+    (first (:rooms decoded))
+    ))
+
+(println (:screen-layout central-cavern))
--- test/manic_miner/core_test.clj
+++ test/manic_miner/core_test.clj
@@ -2,6 +2,10 @@
   (:require [clojure.test :refer :all]
             [manic-miner.core :refer :all]))
 
-(deftest a-test
-  (testing "I don't fail anymore"
-    (is (= 1 1))))
+(deftest good-first-room-name
+  (testing "First room is Central Cavern"
+    (is (= (first manic-miner-room-names) "Central Cavern"))))
+
+(deftest good-last-room-name
+  (testing "Last room is Final Barrier"
+    (is (= (last manic-miner-room-names) "The Final Barrier"))))
