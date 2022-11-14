6539f108134fdb9790eee35421d129d33b0f2e7f
Yvan Aillet <yvanaillet@gmail.com>
2022-11-14 10:39:34 +0100

    nettoyage

--- src/manic_miner/core.clj
+++ src/manic_miner/core.clj
@@ -93,4 +93,35 @@
     (first (:rooms decoded))
     ))
 
-(println (:screen-layout central-cavern))
+(def colour-set
+  "Colour codes used for colour attributes"
+  {
+   2r000 :black
+   2r001 :blue
+   2r010 :red
+   2r011 :magenta
+   2r100 :green
+   2r101 :cyan
+   2r110 :yellow
+   2r111 :white
+   }
+  )
+
+(defn colour-attributes
+  "Get colour attributes from screen layout"
+  [byte-value]
+  {
+   :flash (bit-shift-right (bit-and byte-value 2r10000000) 7)
+   :brightness (bit-shift-right (bit-and byte-value 2r01000000) 6)
+   :paper-colour (get colour-set (bit-shift-right (bit-and byte-value 2r00111000) 3))
+   :ink-colour (get colour-set (bit-shift-right (bit-and byte-value 2r00000111) 0))
+   }
+  )
+
+(let [layout (:screen-layout central-cavern)
+      node-list (map (comp :paper-colour colour-attributes) layout)
+      ]
+  (println node-list)
+  )
+
+
--- test/manic_miner/core_test.clj
+++ test/manic_miner/core_test.clj
@@ -2,6 +2,7 @@
   (:require [clojure.test :refer :all]
             [manic-miner.core :refer :all]))
 
+;; Room names
 (deftest good-first-room-name
   (testing "First room is Central Cavern"
     (is (= (first manic-miner-room-names) "Central Cavern"))))
@@ -9,3 +10,16 @@
 (deftest good-last-room-name
   (testing "Last room is Final Barrier"
     (is (= (last manic-miner-room-names) "The Final Barrier"))))
+
+;; Colour attributes
+(deftest colour-attribute-22
+  (testing "The good colour attribues for 22"
+    (is (= (colour-attributes 22) 
+           {
+            :flash 0
+            :brightness 0
+            :paper-colour :red 
+            :ink-colour :yellow
+            }
+           ))))
+
