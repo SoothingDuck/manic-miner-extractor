d2b651cd868b8fb1e93c888095f5efaa1ea23a81
Orpheus <orpheus@gmail.com>
2023-08-07 22:01:02 +0200

    no mikera

--- src/manic_miner/block.clj
+++ src/manic_miner/block.clj
@@ -3,8 +3,8 @@
    [org.clojars.smee.binary.core :as b]
    [manic-miner.tools :as tools]
    [manic-miner.colour :as mc]
-   [mikera.image.core :as c]
-   [mikera.image.colours :as colour]
+;;   [mikera.image.core :as c]
+;;   [mikera.image.colours :as colour]
    [clojure.pprint :refer (cl-format)]
    [clojure.string :as s]
 
@@ -45,45 +45,45 @@
   )
 
 
-(defn rgb-colour
-  "Retourne la couleur associée au bloc"
-  [block]
-  (let [colour-attribute (:colour-attribute block)
-        detailled-attributes (mc/decode-attributes colour-attribute)
-        ink-colour (:ink-colour detailled-attributes)
-        [r g b] (ink-colour mc/zx-spectrum-color-codes)]
-    (colour/rgb r g b)))
+;;(defn rgb-colour
+;;  "Retourne la couleur associée au bloc"
+;;  [block]
+;;  (let [colour-attribute (:colour-attribute block)
+;;        detailled-attributes (mc/decode-attributes colour-attribute)
+;;        ink-colour (:ink-colour detailled-attributes)
+;;        [r g b] (ink-colour mc/zx-spectrum-color-codes)]
+;;    (colour/rgb r g b)))
 
-(defn make-image
-  "Affiche le bloc"
-  [block]
+;;(defn make-image
+;;  "Affiche le bloc"
+;;  [block]
+;;
+;;  (let [bi (c/new-image 8 8 false)
+;;        mapping (pixel-mapping block)
+;;        ]
+;;
+;;    (doseq [x (range 8)
+;;            y (range 8)]
+;;      (when (get-in mapping [y x])
+;;        (c/set-pixel bi x y (rgb-colour block))
+;;
+;;        )
+;;      )
+;;    ;; fill some random pixels with colours
+;;    ;; (dotimes [i 1024]
+;;    ;;   (aset pixels i (colour/yellow)))
+;;
+;;    ;; update the image with the newly changed pixel values
+;;
+;;    ;; view our new work of art
+;;    ;; the zoom function will automatically interpolate the pixel values
+;;    ;; (c/show bi :zoom 10.0 :title "Isn't it beautiful?")
+;;    bi
+;;    )
+;;  )
 
-  (let [bi (c/new-image 8 8 false)
-        mapping (pixel-mapping block)
-        ]
-
-    (doseq [x (range 8)
-            y (range 8)]
-      (when (get-in mapping [y x])
-        (c/set-pixel bi x y (rgb-colour block))
-
-        )
-      )
-    ;; fill some random pixels with colours
-    ;; (dotimes [i 1024]
-    ;;   (aset pixels i (colour/yellow)))
-
-    ;; update the image with the newly changed pixel values
-
-    ;; view our new work of art
-    ;; the zoom function will automatically interpolate the pixel values
-    ;; (c/show bi :zoom 10.0 :title "Isn't it beautiful?")
-    bi
-    )
-  )
-
-(defn save-image
-  "Sauvegarde l'image à l'emplacement donné"
-  [block filename]
-  (c/save (make-image block) filename)
-  )
+;;(defn save-image
+;;  "Sauvegarde l'image à l'emplacement donné"
+;;  [block filename]
+;;  (c/save (make-image block) filename)
+;;  )
